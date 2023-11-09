c746cab1ccc5c0464a42c7213d00b0c868584b58
Develku <110651468+Develku@users.noreply.github.com>
2023-11-09 11:16:27 +1100

    Update update-readme.py

--- .github/scripts/update-readme.py
+++ .github/scripts/update-readme.py
@@ -1,55 +1,87 @@
 import requests
 from bs4 import BeautifulSoup
 
-base_url = 'https://my-digital-garden-green-seven.vercel.app'
-response = requests.get(base_url)
+# Function to fetch and update blog posts
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Parse the HTML data from the response
-    soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Find the table body in the HTML
-    # NOTE: The actual class or HTML structure may vary, adjust the selector accordingly
-    table_body = soup.find('tbody', {'class': 'table-view-tbody'})
-    rows = table_body.find_all('tr')  # Find all table rows in the table body
+def update_blog_posts():
+    base_url = 'https://my-digital-garden-green-seven.vercel.app'
+    print("Fetching blog posts from:", base_url)
+    try:
+        response = requests.get(base_url)
 
-    # Initialize the blog posts content with a heading
-    blog_posts_markdown = ""
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Parse the HTML data from the response
+            soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Loop over each table row
-    for row in rows:
-        # Extract the title and link from the link element in the row
-        title = row.find('a').text
-        link = base_url + row.find('a')['href']
+            # Find the table body in the HTML
+            # NOTE: The actual class or HTML structure may vary, adjust the selector accordingly
+            table_body = soup.find('tbody', {'class': 'table-view-tbody'})
+            # Find all table rows in the table body
+            rows = table_body.find_all('tr')
 
-        # Add the title and link to the blog posts content
-        blog_posts_markdown += f"- [{title}]({link})\n"
+            # Initialize the blog posts content with a heading
+            blog_posts_markdown = ""
 
-    # Open the README file in read mode
-    with open('README.md', 'r') as f:
-        # Read the README content from the file
-        readme = f.read()
+            # Loop over each table row
+            for row in rows:
+                # Extract the title and link from the link element in the row
+                title = row.find('a').text
+                link = base_url + row.find('a')['href']
 
-    # Define the start and end markers
-    start_marker = "<!--START_SECTION:blog-->"
-    end_marker = "<!--END_SECTION:blog-->"
+                # Add the title and link to the blog posts content
+                blog_posts_markdown += f"- [{title}]({link})\n"
 
-    # Split the README content into parts
-    start_index = readme.find(start_marker) + len(start_marker)
-    end_index = readme.find(end_marker)
+            print("Blog posts fetched successfully.")
+            return blog_posts_markdown
+        else:
+            print(
+                f"Failed to fetch blog posts, status code: {response.status_code}")
+            return None
+    except Exception as e:
+        print(f"An error occurred while fetching blog posts: {e}")
+        return None
 
-    # Check if both markers are present
-    if start_index == -1 or end_index == -1:
-        print("Could not find markers in the README.md file.")
+# Function to update the README file
+
+
+def update_readme(blog_posts_markdown):
+    if blog_posts_markdown:
+        print("Updating README with the latest blog posts.")
+        try:
+            # Open the README file in read mode
+            with open('README.md', 'r') as f:
+                readme = f.read()
+
+            # Define the start and end markers
+            start_marker = "<!--START_SECTION:blog-->"
+            end_marker = "<!--END_SECTION:blog-->"
+
+            # Split the README content into parts
+            start_index = readme.find(start_marker) + len(start_marker)
+            end_index = readme.find(end_marker)
+
+            # Check if both markers are present
+            if start_index == -1 or end_index == -1:
+                print("Could not find markers in the README.md file.")
+            else:
+                # Replace the old list of blog articles with the new one in the README content
+                updated_readme = readme[:start_index] + "\n" + \
+                    blog_posts_markdown + readme[end_index:]
+
+                # Write the updated README content back to the README.md file
+                with open('README.md', 'w') as f:
+                    f.write(updated_readme)
+
+                print("README updated successfully.")
+        except Exception as e:
+            print(f"An error occurred while updating README: {e}")
     else:
-        # Replace the old list of blog articles with the new one in the README content
-        updated_readme = readme[:start_index] + "\n" + \
-            blog_posts_markdown + readme[end_index:]
+        print("No blog posts markdown to update README.")
 
-        # Write the updated README content back to the README.md file
-        with open('README.md', 'w') as f:
-            f.write(updated_readme)
 
-else:
-    print(f"Failed to fetch blog posts, status code: {response.status_code}")
+# Main execution
+if __name__ == "__main__":
+    blog_posts_markdown = update_blog_posts()
+    update_readme(blog_posts_markdown)
